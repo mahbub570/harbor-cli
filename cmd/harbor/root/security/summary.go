9991adf3da8be80d51659f1f39a84fccd53b1ea8
axif <muhamadasif570@gmail.com>
2024-09-15 13:55:12 +0600

    update

---
+++ cmd/harbor/root/security/summary.go
@@ -0,0 +1,26 @@
+package security
+
+import (
+	"fmt"
+
+	"github.com/goharbor/harbor-cli/pkg/api"
+	"github.com/goharbor/harbor-cli/pkg/views/security/summary"
+	"github.com/spf13/cobra"
+)
+ 
+func getSecuritySummaryCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "summary",
+		Short: "Get the security summary of the system",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			response,err := api.GetSecuritySummary()
+			if err != nil {
+				return fmt.Errorf("error getting security summary: %w", err)
+			}
+			summary.SecuritySummary(response.Payload)
+			return nil
+		},
+	}
+
+	return cmd
+}
---
+++ pkg/api/security_handler.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	"github.com/goharbor/go-client/pkg/sdk/v2.0/client/securityhub"
+	"github.com/goharbor/harbor-cli/pkg/utils"
+)
+
+func GetSecuritySummary() (*securityhub.GetSecuritySummaryOK, error) {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return nil, err
+	}
+ 
+	response, err := client.Securityhub.GetSecuritySummary(ctx,&securityhub.GetSecuritySummaryParams{})
+	if err != nil {
+		return nil,err
+	}
+
+	return response,nil
+}
+
+func ListVulnerabilities(query string) error {
+	ctx, client, err := utils.ContextWithClient()
+	if err != nil {
+		return err
+	}
+
+	params := &securityhub.ListVulnerabilitiesParams{
+		Q: &query,
+	}
+
+	response, err := client.Securityhub.ListVulnerabilities(ctx, params)
+	if err != nil {
+		return err
+	}
+
+	utils.PrintPayloadInJSONFormat(response.Payload)
+	return nil
+}
